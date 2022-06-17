f405c6f542ae4dec2ba32042abf029c7d0ebd876
Arthur Soares <arthurvsp97@gmail.com>
2022-06-17 19:24:45 -0300

    Removing repeated string splits

--- src/arthursoares/dev/compareversion/Program.java
+++ src/arthursoares/dev/compareversion/Program.java
@@ -5,7 +5,7 @@ public class Program
 	public static void main(String[] args)
 	{
 		var versionComparer = new VersionComparer();
-		var result = versionComparer.compareVersions("1.2", "1.2.0.1");
+		var result = versionComparer.compareVersions("1.3", "1.3.0");
 		
 		System.out.println(result);
 	}
--- src/arthursoares/dev/compareversion/VersionComparer.java
+++ src/arthursoares/dev/compareversion/VersionComparer.java
@@ -4,75 +4,72 @@ public class VersionComparer
 {
 	public int compareVersions(String version1, String version2)
 	{
-		var maxRevisionLevels = getMaxRevisionLevels(version1, version2);
-		var revisionLevelLengths = getRevisionLevelsLengths(version1, version2, maxRevisionLevels);
+		var splittedVersion1 = version1.split("\\.");
+		var splittedVersion2 = version2.split("\\.");
 		
-		var version1AsInteger = convertVersionStringToInt(version1, revisionLevelLengths);
-		var version2AsInteger = convertVersionStringToInt(version2, revisionLevelLengths);
+		var maxRevisionLevels = getMaxRevisionLevels(
+				splittedVersion1,
+				splittedVersion2);
+		var revisionLevelLengths = getRevisionLevelsLengths(
+				splittedVersion1,
+				splittedVersion2,
+				maxRevisionLevels);
+		
+		var version1AsInteger = convertVersionStringToInt(splittedVersion1, revisionLevelLengths);
+		var version2AsInteger = convertVersionStringToInt(splittedVersion2, revisionLevelLengths);
 		
 		if (version1AsInteger == version2AsInteger)
 		{
 			return 0;
 		}
 		
-		if (version1AsInteger > version2AsInteger)
-		{
-			return 1;
-		}
-		
-		return -1;
+		return version1AsInteger > version2AsInteger ? 1 : -1;
 	}
 	
-	private int getMaxRevisionLevels(String version1, String version2)
+	private int getMaxRevisionLevels(String[] splittedVersion1, String[] splittedVersion2)
 	{
-		var version1Levels = version1.split("\\.");
-		var version2Levels = version2.split("\\.");
-		
-		return version1Levels.length > version2Levels.length ?
-			version1Levels.length :
-			version2Levels.length;
+		return splittedVersion1.length > splittedVersion2.length ?
+			splittedVersion1.length :
+			splittedVersion2.length;
 	}
 	
-	private int[] getRevisionLevelsLengths(String version1, String version2, int maxRevisionLevels)
+	private int[] getRevisionLevelsLengths(String[] splittedVersion1, String[] splittedVersion2, int maxRevisionLevels)
 	{
 		var lengths = new int[maxRevisionLevels];
 		
-		var version1Levels = version1.split("\\.");
-		for (var index = 0; index < version1Levels.length; index++)
-		{
-			lengths[index] = version1Levels[index].length();
-		}
 		
-		var version2Levels = version2.split("\\.");
-		for (var index = 0; index < version2Levels.length; index++)
+		for (var index = 0; index < splittedVersion1.length; index++)
 		{
-			if (lengths[index] < version2Levels[index].length())
+			lengths[index] = splittedVersion1[index].length();
+		}
+		for (var index = 0; index < splittedVersion2.length; index++)
+		{
+			if (lengths[index] < splittedVersion2[index].length())
 			{
-				lengths[index] = version2Levels[index].length();				
+				lengths[index] = splittedVersion2[index].length();				
 			}
 		}
 		
 		return lengths;
 	}
 	
-	private int convertVersionStringToInt(String version, int[] revisionLevelLengths)
+	private int convertVersionStringToInt(String[] splittedVersion, int[] revisionLevelLengths)
 	{
-		var versionLevels = version.split("\\.");
 		var versionNumber = "";
 		
 		for (var index = 0; index < revisionLevelLengths.length; index++)
 		{
-			if (index < versionLevels.length)
+			if (index < splittedVersion.length)
 			{
-				var revisionLevel = versionLevels[index];
+				var revisionLevel = splittedVersion[index];
 				var levelLength = revisionLevel.length();
 				versionNumber += generateZerosString(revisionLevelLengths[index] - levelLength);
 				versionNumber += revisionLevel;
+				
+				continue;
 			}
-			else
-			{
-				versionNumber += generateZerosString(revisionLevelLengths[index]);
-			}
+			
+			versionNumber += generateZerosString(revisionLevelLengths[index]);
 		}
 		
 		return Integer.parseInt(versionNumber);
